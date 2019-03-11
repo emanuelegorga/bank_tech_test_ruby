102615a96b39d301464e1f75e0476ed30ce1d868
Emanuele <emanuelegorga@gmail.com>
2019-03-11 12:50:39 +0000

    User can withdraw money for their account and balance is updated

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,4 +9,8 @@ class BankAccount
   def deposit(amount)
     @balance += amount
   end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,9 +7,12 @@ RSpec.describe BankAccount do
     expect(account.balance).to eq(0)
   end
 
-  it 'can deposit money and the balance gets updated' do
-    account.deposit(50)
-    expect(account.balance).to eq(50)
+  it 'can deposit money and the balance is updated' do
+    expect{ account.deposit(50) }.to change { account.balance }.by(50)
+  end
+
+  it 'can withdraw money and the balance is updated' do
+    expect{ account.withdraw(20) }.to change { account.balance }.by(-20)
   end
 
 end
