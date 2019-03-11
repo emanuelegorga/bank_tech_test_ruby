f7f9bd26f01b8a45e58da661fa21be9d61a851b7
Emanuele <emanuelegorga@gmail.com>
2019-03-11 19:27:47 +0000

    Transactions are stored in a history array within BankAccount object

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,21 +1,21 @@
 require_relative 'transaction'
 
 class BankAccount
+  attr_reader :balance, :history
 
-  attr_reader :balance
-
-  def initialize(balance = 0)
+  def initialize(history = [], balance = 0)
     @balance = balance
+    @history = history
   end
 
   def deposit(amount, transaction=Transaction)
     @balance += amount
-    transaction.new(amount, @balance, 'deposit')
+    @history.push(transaction.new(amount, @balance, 'deposit'))
   end
 
   def withdraw(amount, transaction=Transaction)
     raise("You cannot exceed your balance") if amount > @balance
     @balance -= amount
-    transaction.new(amount, @balance, 'withdraw')
+    @history.push(transaction.new(amount, @balance, 'withdraw'))
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,7 +1,10 @@
 require 'bank_account'
 
 RSpec.describe BankAccount do
-  subject(:account) { described_class.new }
+  subject(:account) { described_class.new(history) }
+  let(:history) { [] }
+  let(:makeDeposit) { account.deposit(20) }
+  let(:makeWithdraw) { account.withdraw(15) }
 
   it 'has a default balance of zero' do
     expect(account.balance).to eq(0)
@@ -12,12 +15,21 @@ RSpec.describe BankAccount do
   end
 
   it 'can withdraw money and the balance is updated' do
-    account.deposit(20)
+    makeDeposit
     expect{ account.withdraw(20) }.to change { account.balance }.by(-20)
   end
 
   it 'user withdraw cannot exceed balance' do
-    expect {account.withdraw(10) }.to raise_error('You cannot exceed your balance')
+    expect { account.withdraw(10) }.to raise_error('You cannot exceed your balance')
+  end
+
+  it 'pushes a deposit transaction into history array' do
+    expect { makeDeposit }.to change { history.length }.by(1)
+  end
+
+  it 'pushes a deposit transaction into history array' do
+    makeDeposit
+    expect { makeWithdraw }.to change { history.length }.by(1)
   end
 
 end
