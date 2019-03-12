4393c1dd2b8e45fc2e9dd9a43171e418cf0934fe
Emanuele <emanuelegorga@gmail.com>
2019-03-12 17:41:19 +0000

    Refactored Statement Class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,15 +1,39 @@
 class Statement
 
   def printStatement(transactions)
-    header = "date  ||  credit  ||  debit  ||  balance\n"
+    @header = "date  ||  credit  ||  debit  ||  balance\n"
     transactions.each do |transaction|
-      if transaction.type == 'deposit'
-        header += transaction.date + "  ||  " + transaction.amount.to_s + "  ||  ||  " + transaction.balance.to_s + "\n"
-      else
-        header += transaction.date + "  ||  ||  " + transaction.amount.to_s + "  ||  " + transaction.balance.to_s + "\n"
-      end
+      transaction.type == 'deposit' ? deposit(transaction) : withdrawal(transaction)
     end
-    return header
+    return @header
+  end
+
+  private
+
+  def deposit(transaction)
+    date(transaction)
+    amount(transaction)
+    @header += "||  "
+    balance(transaction)
+  end
+
+  def withdrawal(transaction)
+    date(transaction)
+    @header += "||  "
+    amount(transaction)
+    balance(transaction)
+  end
+
+  def date(transaction)
+    @header += transaction.date + "  ||  "
+  end
+
+  def amount(transaction)
+    @header += transaction.amount.to_s + "  ||  "
+  end
+
+  def balance(transaction)
+    @header += transaction.balance.to_s + "\n"
   end
 
 end
