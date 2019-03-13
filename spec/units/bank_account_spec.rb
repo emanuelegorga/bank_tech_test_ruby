c2616cca257c384fb7ead1f53f4ca4fd53b8e43a
Emanuele <emanuelegorga@gmail.com>
2019-03-13 08:38:56 +0000

    Updated tests in bankaccount

--- spec/units/bank_account_spec.rb
+++ spec/units/bank_account_spec.rb
@@ -1,8 +1,8 @@
 require 'bank_account'
 
 RSpec.describe BankAccount do
-  subject(:account) { described_class.new(history, statement) }
-  let(:history) { double("TransactionsHistory") }
+  subject(:account) { described_class.new(transactionHistory, statement) }
+  let(:transactionHistory) { double("TransactionsHistory") }
   let(:statement) { double("Statement") }
   let(:makeDeposit) { account.deposit(20) }
   let(:makeWithdraw) { account.withdraw(15) }
@@ -13,25 +13,25 @@ RSpec.describe BankAccount do
 
   describe '#deposit transactions' do
     it 'can deposit money and the balance is updated' do
-      allow(history).to receive(:add)
-      expect{ account.deposit(50) }.to change { account.balance }.by(50)
+      allow(transactionHistory).to receive(:add)
+      expect{ makeDeposit }.to change { account.balance }.by(20)
     end
   end
 
   describe '#withdraw transactions' do
     it 'can withdraw money and the balance is updated' do
-      allow(history).to receive(:add)
+      allow(transactionHistory).to receive(:add)
       makeDeposit
-      expect{ account.withdraw(20) }.to change { account.balance }.by(-20)
+      expect{ makeWithdraw }.to change { account.balance }.by(-15)
     end
 
     it 'user withdraw cannot exceed balance' do
-      expect { account.withdraw(10) }.to raise_error('You cannot exceed your balance')
+      expect { makeWithdraw }.to raise_error('You cannot exceed your balance')
     end
   end
 
   it 'prints a statement' do
-    allow(history).to receive(:history)
+    allow(transactionHistory).to receive(:history)
     allow(statement).to receive(:printStatement).and_return("date  ||  credit  ||  debit  ||  balance\n03/12/2019  ||  30  ||  ||  30\n03/12/2019  ||  ||  10  ||  20\n")
     expect(account.statement).to eq("date  ||  credit  ||  debit  ||  balance\n03/12/2019  ||  30  ||  ||  30\n03/12/2019  ||  ||  10  ||  20\n")
   end
